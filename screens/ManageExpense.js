7de429d4bd04f485721034833970775f1e0be04c
Dhiraj Powar <dhiraj.powar210@gmail.com>
2024-10-28 12:49:42 +0530

    expense id passed to expense item

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,17 @@
 import { Text } from "react-native";
+import { useLayoutEffect } from "react";
 
-function ManageExpense() {
+function ManageExpense({route, navigation }) {
+
+    const expenseEditId = route.params?.expenseId;
+
+    const isEditing = !!expenseEditId;
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? 'Edit Expense' : 'Add Expense',
+        });
+    }, [navigation, isEditing])
     return <Text>Manage expense</Text>;
 }
 
