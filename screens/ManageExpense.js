eb041a66a28f87f6b27a7a4bb85793a5d510c6d2
Dhiraj Powar <dhiraj.powar210@gmail.com>
2024-10-29 16:31:15 +0530

    custom button added

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,8 @@
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useLayoutEffect } from "react";
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
+import Button from "../components/UI/Button";
 
 function ManageExpense({route, navigation }) {
 
@@ -12,7 +15,46 @@ function ManageExpense({route, navigation }) {
             title: isEditing ? 'Edit Expense' : 'Add Expense',
         });
     }, [navigation, isEditing])
-    return <Text>Manage expense</Text>;
+
+    function deleteExpenseHandler(){}
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                <Button mode='flat'>Cancel</Button>
+                <Button>{isEditing ? 'Update' : 'Add'}</Button>
+            </View>
+            {isEditing &&
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon="trash"
+                        color={GlobalStyles.colors.error500}
+                        size={36}
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+                
+                }
+        </View>
+    );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center'
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    }
+})
